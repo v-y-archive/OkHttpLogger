19c3601883a4d2fad50da5cc4c632aca54d68a24
Vadim Yegorov <vadimszzz@mail.ru>
2022-06-03 23:26:04 +0300

    Xposed gradle project

---
+++ XposedApp/app/src/main/java/my/xposedapp/modules/TestModule.java
@@ -0,0 +1,14 @@
+package my.xposedapp.modules;
+
+import android.util.Log;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class TestModule implements IXposedHookLoadPackage {
+
+}
