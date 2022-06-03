2803c5fd7c7a1bee90b3d52b69f80a931a902f91
Vadim Yegorov <vadimszzz@mail.ru>
2022-06-04 00:00:37 +0300

    Scripts updated

--- preinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'RUN android-24/google_apis/x86 EMULATOR FIRST WITH -no-snapshot option'
-cd $(dirname $0)/preinstall/rootAVD
-sh rootAVD.sh ${ANDROID_SDK_ROOT}/system-images/android-24/google_apis_playstore/x86/ramdisk.img
-cd -
